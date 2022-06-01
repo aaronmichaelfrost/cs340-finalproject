81a262eb0959292c98229548d671260d483e4c2c
aaronmichaelfrost <aaronmichaelfrost@gmail.com>
2022-06-01 16:28:38 -0700

    wip

--- DDQ.sql
+++ DDQ.sql
@@ -1,4 +1,36 @@
+
+DROP TABLE IF EXISTS `OrderProducts`;
 DROP TABLE IF EXISTS `Orders`;
+DROP TABLE IF EXISTS Customers;
+DROP TABLE IF EXISTS Products;
+
+
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE Customers (
+  customerId int(9) NOT NULL AUTO_INCREMENT,
+  email varchar(255) NOT NULL,
+  cell int(10),
+  firstName varchar(255),
+  lastName varchar(255),
+  birthDate date,
+
+  PRIMARY KEY (customerId)
+  ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE Products (
+  productId int(9) NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  price decimal unsigned,
+  PRIMARY KEY (productId)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+
 
 CREATE TABLE IF NOT EXISTS `Orders` (
 
@@ -10,11 +42,11 @@ CREATE TABLE IF NOT EXISTS `Orders` (
 
 	PRIMARY KEY (`orderId`),
 
-	FOREIGN KEY (`customerId`) REFERENCES `Customer` (`customerId`)
+	FOREIGN KEY (`customerId`) REFERENCES `Customers` (`customerId`)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `OrderProducts`;
+
 
 CREATE TABLE IF NOT EXISTS `OrderProducts` (
 
@@ -22,7 +54,9 @@ CREATE TABLE IF NOT EXISTS `OrderProducts` (
 
 	`orderId` int(11) NOT NULL,
     
-	`productId` int(11) NOT NULL,   
+	`productId` int(11) NOT NULL,  
+
+	`quantity` int(5),
 
 	PRIMARY KEY (`orderProductId`), 
 
@@ -32,40 +66,11 @@ CREATE TABLE IF NOT EXISTS `OrderProducts` (
 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
-
-DROP TABLE IF EXISTS Customers;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-CREATE TABLE Customers (
-  customerId int(9) NOT NULL AUTO_INCREMENT,
-  email varchar(255) NOT NULL,
-  cell int(10),
-  firstName varchar(255),
-  lastName varchar(255),
-  birthDate datetime,
-
-  PRIMARY KEY (customerId)
-  ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
-
-
-DROP TABLE IF EXISTS Products;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-CREATE TABLE Products (
-  productId int(9) NOT NULL AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
-  price decimal unsigned,
-  PRIMARY KEY (productId)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
-
 LOCK TABLES Customers WRITE;
 
-INSERT INTO Customers VALUES ('1','hello@gmail.com','541777444','John','Oranu'),
-(2,'hello@gmail.com','534777444','Mike','Oranu'),
-(3,'hello@gmail.com','541342444','Peter','Hemsworth');
+INSERT INTO Customers VALUES (1,'hello@gmail.com','541777444','John','Oranu','2001-12-11'),
+(2,'hello@gmail.com','534777444','Mike','Oranu','2001-12-11'),
+(3,'hello@gmail.com','541342444','Peter','Hemsworth','2001-12-11');
 
 UNLOCK TABLES;
 
@@ -79,15 +84,15 @@ UNLOCK TABLES;
 
 INSERT INTO Orders (date, customerId) VALUES (
 	'2000-01-01 10:45:23',
-    (SELECT customerId FROM Customer WHERE email='hello@gmail.com' AND cell='541777444' AND  firstName='John' AND lastName='Oranu')
+    (SELECT customerId FROM Customers WHERE email='hello@gmail.com' AND cell='541777444' AND  firstName='John' AND lastName='Oranu')
 );
 INSERT INTO Orders (date, customerId) VALUES (
 	'2005-02-10 10:45:23',
-	(SELECT customerId FROM Customer WHERE email='hello@gmail.com' AND cell='534777444' AND  firstName='Mike' AND lastName='Oranu')
+	(SELECT customerId FROM Customers WHERE email='hello@gmail.com' AND cell='534777444' AND  firstName='Mike' AND lastName='Oranu')
 );
 INSERT INTO Orders (date, customerId) VALUES (
 	'2009-03-20 10:45:23',
-	(SELECT customerId FROM Customer WHERE email='hello@gmail.com' AND cell='541342444' AND  firstName='Peter' AND lastName='Hemsworth')
+	(SELECT customerId FROM Customers WHERE email='hello@gmail.com' AND cell='541342444' AND  firstName='Peter' AND lastName='Hemsworth')
 );
 
 INSERT INTO OrderProducts (orderId, productId, quantity) VALUES (
